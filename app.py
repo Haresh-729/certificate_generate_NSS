ae43a034c20f2c627e0ee6d9b8f0bcd49e1fe631
Haresh Kurade <kuradeharesh4002@gmail.com>
2024-08-29 00:02:53 +0530

    deployment changes.

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
